14be6af7428c1b4eeacd75bbb38922d326b8ee00
williamalvesprado <williamalvesprado@hotmail.com>
2020-09-25 14:12:54 -0300

    For decrescente

---
+++ exercicios/src/controle/For2.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		//for decrescente
+		for (int i = 10; i >= 0; i-=2)
+			
+		{
+
+			System.out.print(i + " ");
+
+		}
+	}
+}
